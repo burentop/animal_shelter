a491daaebf921e37b5cfbcb187cf4c72279fac6b
Brent Perry <burentoperii@gmail.com>
2018-05-03 10:14:06 -0500

    add methods for adoption and IsACat

--- Animal.cs
+++ Animal.cs
@@ -5,6 +5,17 @@ public class Animal
 {
   public int Age;
   public string Species;
+  public bool Adopted;
+
+  public bool IsACat() 
+  {
+    return Species == "cat";
+  }
+
+  public void ChangeAdoption()
+  {
+    Adopted = !Adopted;
+  }
 }
 
 public class Program
@@ -14,16 +25,24 @@ public class Program
     Animal vader = new Animal();
     vader.Age = 15;
     vader.Species = "Black Lab";
+    vader.Adopted = true;
 
     Animal frank = new Animal();
     frank.Age = 5;
     frank.Species = "Tabby Cat";
+    frank.Adopted = false;
 
     Animal pal = new Animal();
     pal.Age = 7;
     pal.Species = "Mutt";
+    pal.Adopted = false;
 
-    List<Animal> Animals = new List<Animal>() { vader, frank, pal };
+    Animal bob = new Animal();
+    bob.Age = 3;
+    bob.Species = "cat";
+    bob.Adopted = false;
+
+    List<Animal> Animals = new List<Animal>() { vader, frank, pal, bob };
 
     Console.WriteLine("Enter maximum age: ");
     string stringMaxAge = Console.ReadLine();
@@ -51,5 +70,35 @@ public class Program
         Console.WriteLine(pet.Species);
       }
     }
+
+    Console.WriteLine("Here are the cats: ");
+    foreach (Animal pet in Animals)
+    {
+      if (pet.IsACat())
+      {
+        Console.WriteLine(pet.Species);
+      }
+    }
+
+    Console.WriteLine("Here are the pets not adopted yet: ");
+    foreach (Animal pet in Animals)
+    {
+      if (pet.Adopted == false)
+      {
+        Console.WriteLine(pet.Species);
+      }
+    }
+
+    bob.ChangeAdoption();
+
+    Console.WriteLine("One pet was adopted. Here are the pets not adopted yet: ");
+    foreach (Animal pet in Animals)
+    {
+      if (pet.Adopted == false)
+      {
+        Console.WriteLine(pet.Species);
+      }
+    }
+
   }
 }
